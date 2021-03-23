f787a21e86b8336f332d2c623dba9a2cd1e91547
DenisAndrieiew <eretarmineret@gmail.com>
2021-03-23 21:20:51 +0200

    delete unused files

--- src/main/java/Element.java
+++
@@ -1,8 +0,0 @@
-import java.util.concurrent.CyclicBarrier;
-
-public abstract class Element extends Thread {
-    @Override
-    public void run(){
-        System.out.println(Thread.currentThread());
-    }
-}
